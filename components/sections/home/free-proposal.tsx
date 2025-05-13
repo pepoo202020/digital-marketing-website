89acefb7fa6301650e0ad9d95d4a0ca685923ef9
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2025-05-13 18:51:35 +0300

    Completed

--- components/sections/home/free-proposal.tsx
+++ components/sections/home/free-proposal.tsx
@@ -8,8 +8,7 @@ export const FreeProposal = () => {
           {/* Content */}
           <div className="flex flex-col justify-start items-start gap-4 md:gap-6 max-w-full md:max-w-[500px] z-10">
             <h2 className="text-2xl md:text-3xl font-medium text-black">
-              // Change from: Let's talk // To: Let&apos;s talk Let's make
-              things happen
+              Let&apos;s make things happen
             </h2>
             <p className="text-base md:text-lg text-black">
               Contact us today to learn more about how our digital marketing
